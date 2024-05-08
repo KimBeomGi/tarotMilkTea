f5debebcd819a674bfff05485ed24280d470caaf
김범기 <dyd1308@naver.com>
2024-05-08 22:59:40 +0900

    "타로설명 테이블 작성 완료 및 보완"

--- frontend/tarotmilktea/src/components/Layout/CardExplainBody1.tsx
+++ frontend/tarotmilktea/src/components/Layout/CardExplainBody1.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import { Outlet, Link } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from '../../store/hooks'
 import AboutTarot from "./AboutTarot.json"
+import aboutTarotImg1 from "../../assets/images/aboutTarotImg1.png"
 
 import "./CardExplainBody1.css"
 
@@ -10,18 +11,65 @@ function CardExplainBody1() {
   // const count = useAppSelector((state) => state.counter.value)
   const dispatch = useAppDispatch()
   const aboutTarot = useState(AboutTarot)
+  const historyPDiv = useRef<HTMLDivElement>(null)
+  const historyImgDiv = useRef<HTMLDivElement>(null)
+  const [historyPDivHeight, setHistoryPDivHeight] = useState(0); // historyPDiv의 높이를 저장할 상태 변수를 생성합니다.
+  const [bH, setBH] = useState<number>(window.innerHeight)
+  const [bW, setBW] = useState<number>(window.innerWidth)
+
+  useEffect(() => {
+    const updateHeightAndWidth = () => {
+      setBH(window.innerHeight);
+      setBW(window.innerWidth);
+  
+      if (historyPDiv.current && historyImgDiv.current) {
+        if (window.innerWidth >= 1024) {
+          const newHeight = historyPDiv.current?.offsetHeight;
+          historyImgDiv.current.style.height = `${newHeight}px`;
+          historyImgDiv.current.style.width = 'auto';
+          historyImgDiv.current.style.maxWidth = `${window.innerWidth / 2}px`;
+        } else if(window.innerWidth <= 768){
+          historyImgDiv.current.style.height = 'auto';
+          historyImgDiv.current.style.width = `${window.innerWidth * 0.7}px`;
+        }else if(window.innerWidth < 1024) {
+          historyImgDiv.current.style.height = 'auto';
+          historyImgDiv.current.style.width = `${window.innerWidth}px`;
+        }
+      }
+    };
+  
+    window.addEventListener('resize', updateHeightAndWidth);
+    // 초기 설정을 위해 직접 호출
+    updateHeightAndWidth();
+  
+    return () => {
+      window.removeEventListener('resize', updateHeightAndWidth);
+    };
+  }, [])
   
 
   return (
     <div className="CardExplainBody1">
       <div className='cEB1-shape'>
-        <h1>타로 역사</h1>
-        <div>
-          {aboutTarot[0].tarotHistory.map((v1, i1) => (
-            <p key={i1}>{v1}</p>
-          ))}
+        <h1 className='cEB1-shape-history-title'>타로 카드의 역사</h1>
+        <div className='cEB1-shape-history-content'>
+          <div 
+            className='cEB1-shape-history-content-imgdiv'
+            ref={historyImgDiv}
+          >
+            <img 
+              src={aboutTarotImg1} 
+              alt="대중적인 웨이트 타로 카드" 
+              className='cEB1-shape-history-content-img' 
+            />
+          </div>
+          <div className='cEB1-shape-history-content-paragraphdiv' ref={historyPDiv}>
+            {aboutTarot[0].tarotHistory.map((v1, i1) => (
+              <p key={i1}>{v1}</p>
+            ))}
+          </div>
         </div>
-        <h1>타로 구성</h1>
+        <h1>타로 카드 구성</h1>
         <div>
           {aboutTarot[0].tarotConfiguration.map((v2, i2) => (
               <p key={i2}>{v2}</p>
@@ -29,8 +77,12 @@ function CardExplainBody1() {
         </div>
         <h1>타로 카드별 의미</h1>
         <h2>메이저 타로 카드</h2>
-        <div>
-          <table>
+        <div
+          className='majorTarot-table-div'
+        >
+          <table
+            className='majorTarot-table'
+          >
             <thead>
               <tr>
                 <th>번호</th>
@@ -41,22 +93,16 @@ function CardExplainBody1() {
             </thead>
             <tbody>
               {aboutTarot[0].majorTarotMeaning.map((v3, i3) => (
-                <tr key={i3}>
+                <tr key={i3}
+                  onClick={() => {console.log(v3.number)}}
+                >
                   <td>{v3.number}</td>
                   <td>{v3.name}</td>
                   <td>
-                    {v3.forward.map((v4, i4) => (
-                    <span key={i4}>
-                      {v4}
-                    </span>
-                    ))}
+                    {v3.forward.join(", ")}
                   </td>
                   <td>
-                    {v3.reverse.map((v5, i5) => (
-                      <span key={i5}>
-                        {v5}
-                      </span>
-                    ))}
+                    {v3.reverse.join(", ")}
                   </td>
                 </tr>
               ))}
@@ -65,8 +111,12 @@ function CardExplainBody1() {
         </div>
 
         <h2>마이너 타로 카드</h2>
-        <div>
-          <table>
+        <div 
+          className='minorTarot-table-div'
+        >
+          <table
+            className='minorTarot-table'
+          >
             <thead>
               <tr>
                 <th>번호</th>
@@ -77,36 +127,24 @@ function CardExplainBody1() {
               </tr>
             </thead>
             <tbody>
-              {aboutTarot[0].minorTarotMeaning.map((v6, i6) => (
-                <tr key={i6}>
-                  <td>{v6.number}</td>
+              {aboutTarot[0].minorTarotMeaning.map((v4, i4) => (
+                <tr key={i4}
+                  onClick={() => {
+                    console.log(v4.number)
+                  }}
+                >
+                  <td>{v4.number}</td>
                   <td>
-                    {v6.wands.map((v7, i7) => (
-                      <span key={i7}>
-                        {v7}
-                      </span>
-                    ))}
+                    {v4.wands.join(", ")}
                   </td>
                   <td>
-                    {v6.cups.map((v8, i8) => (
-                      <span key={i8}>
-                        {v8}
-                      </span>
-                    ))}
+                    {v4.cups.join(", ")}
                   </td>
                   <td>
-                    {v6.swords.map((v9, i9) => (
-                      <span key={i9}>
-                        {v9}
-                      </span>
-                    ))}
+                    {v4.swords.join(", ")}
                   </td>
                   <td>
-                    {v6.pentacles.map((v10, i10) => (
-                      <span key={i10}>
-                        {v10}
-                      </span>
-                    ))}
+                    {v4.pentacles.join(", ")}
                   </td>
                 </tr>
               ))}
