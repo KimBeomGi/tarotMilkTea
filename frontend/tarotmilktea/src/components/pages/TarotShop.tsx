11d6c67407e3e9cf9cb6229c679212693e0ba37e
김범기 <dyd1308@naver.com>
2024-05-26 04:48:53 +0900

    "카카오맵 위치 변경시 목록 생성 구현중,  위치변경에 따른 정보받아오기 완료"

--- frontend/tarotmilktea/src/components/pages/TarotShop.tsx
+++ frontend/tarotmilktea/src/components/pages/TarotShop.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useSyncExternalStore} from 'react';
 
 import { Outlet, Link } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from '../../store/hooks'
@@ -50,11 +50,13 @@ function TarotShop() {
 
   // 카카오 맵 api 적용
   // 지도 중심 위치
-  const [defaultLoc, setDefaultLoc] = useState([33.450701, 126.570667])
-  const [gpsLocation, setGpsLocation] = useState([33.450701, 126.570667])
-  const [myLocation, setMyLocation] = useState([33.450701, 126.570667])
+  const [defaultLoc, setDefaultLoc] = useState([37.566826, 126.9786567])
+  const [gpsLocation, setGpsLocation] = useState([37.566826, 126.9786567])
+  const [myLocation, setMyLocation] = useState([37.566826, 126.9786567])
 
-  const [map, setMap] = useState<any>(null); // map 객체를 상태로 관리 react 사용하기위해서
+  const [map, setMap] = useState<any>(null) // map 객체를 상태로 관리 react 사용하기위해서
+  const [isMapMade, setIsMapMade] = useState<boolean>(false)
+  
   // 테스트중
   const [mapInfo, setMapInfo] = useState({ level: null, lat: null, lng: null });
 
@@ -66,10 +68,11 @@ function TarotShop() {
       center: new kakao.maps.LatLng(myLocation[0], myLocation[1]), //지도의 중심좌표.
       // center: new kakao.maps.LatLng(0, 0), //지도의 중심좌표.
       level: 4 //지도의 레벨(확대, 축소 정도)
+
     };
     // let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
     let createdMap = new window.kakao.maps.Map(container, options); // 지도 생성
-    setMap(createdMap)
+    
 
     // map.setDraggable(false)
     // map.setZoomable(false)
@@ -92,14 +95,16 @@ function TarotShop() {
         
     } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
         
-        let locPosition = new kakao.maps.LatLng(defaultLoc)
+        let locPosition = new kakao.maps.LatLng(defaultLoc[0], defaultLoc[1])
         // map.setCenter(locPosition)
         createdMap.setCenter(locPosition)
         setMyLocation(defaultLoc)
     }
 
+    // 지도 영역 변경여부
     window.kakao.maps.event.addListener(createdMap, 'center_changed', () => {
       const level = createdMap.getLevel();
+      
       const latlng = createdMap.getCenter();
 
       setMapInfo({
@@ -108,25 +113,82 @@ function TarotShop() {
         lng: latlng.getLng(),
       });
       setMyLocation([latlng.getLat(), latlng.getLng()])
-    });
-    
+    });  
+    setMap(createdMap)
+        
     /////////// 지도 확대 축소시 문제 때문에 return으로 언마운트시 container를 비워버림
     return () => {
-      if(container){container.innerHTML = '';}
+      if(container){
+        container.innerHTML = '';
+      }
+      setMap(null)
     }
   }, [])
+
+  //////////////
+  //지도 생성 완료 후 시행할 것들
+  useEffect(() => {
+    if(map){
+      let bounds = map.getBounds();
+      let swLatlng = bounds.getSouthWest();
+      let neLatlng = bounds.getNorthEast();
+      // 지도가 이동, 확대, 축소로 인해 지도영역이 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+      kakao.maps.event.addListener(map, 'bounds_changed', function() {             
+          
+        // 지도 영역정보를 얻어옵니다 
+        bounds = map.getBounds();
+        
+        // 영역정보의 남서쪽 정보를 얻어옵니다 
+        swLatlng = bounds.getSouthWest();
+        
+        // 영역정보의 북동쪽 정보를 얻어옵니다 
+        neLatlng = bounds.getNorthEast();
+
+        let sw = new kakao.maps.LatLng(swLatlng.Ma, swLatlng.La),
+        ne = new kakao.maps.LatLng(neLatlng.Ma, neLatlng.La);
+
+        let search_bounds = new kakao.maps.LatLngBounds(sw, ne); // 인자를 주지 않으면 빈 영역을 생성한다.
+
+        let places = new kakao.maps.services.Places();
+        let callback = function(result :any, status :any) {
+          if (status === kakao.maps.services.Status.OK) {
+            console.log(result);
+          }
+        };
+        places.keywordSearch('타로', callback, {
+          // category_group_code : "CE7",
+          bounds: search_bounds,
+          useMapCenter : true,
+          size : 15,
+        });
+      });
+    }
+    
+  }, [map])
+  /////////////
   
+
+
+
   /////////////////////////////////////////////////////
   // 지도 중심 좌표 변화 이벤트를 등록한다
   const zoomIn = () => {
     if (map) { // map 객체가 초기화된 경우에만 실행
-      map.setLevel(map.getLevel() - 1);
+      map.setLevel(map.getLevel() - 1, {
+        animate: {
+          duration: 300
+        }
+      });
     }
   }
 
   const zoomOut = () => {
     if (map) { // map 객체가 초기화된 경우에만 실행
-      map.setLevel(map.getLevel() + 1);
+      map.setLevel(map.getLevel() + 1, {
+        animate: {
+          duration: 300
+        }
+      });
     }
   }
   const panTo = () => {
