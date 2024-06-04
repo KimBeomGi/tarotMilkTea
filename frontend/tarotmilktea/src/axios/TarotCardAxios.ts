00f71b50c6e640daee0595afbca3f5a9cc3e13cd
김범기 <dyd1308@naver.com>
2024-06-05 01:28:55 +0900

    "react의 서버연동 준비"

--- frontend/tarotmilktea/src/axios/TarotCardAxios.ts
+++ frontend/tarotmilktea/src/axios/TarotCardAxios.ts
@@ -3,32 +3,86 @@ import axios from "axios";
 const base_url = "http://127.0.0.1:8000/"
 
 export async function getTarotCard() { // async, await을 사용하는 경우
-    try {
-        const response = await axios.get(`${base_url}tarotcard/`); // Backtick(`)을 이용해 이렇게 요청할 수도 있다.
-        
-        return response
-    } catch (e) {
-        // 실패 시 처리
-        console.error(e);
-    }
+  try {
+    const response = await axios.get(`${base_url}tarotcard/`); // Backtick(`)을 이용해 이렇게 요청할 수도 있다.
+    
+    return response
+  } catch (e) {
+    // 실패 시 처리
+    console.error(e);
+  }
 }
 
-
+// GeminiApi를 이용한 운세 보기
 export async function getReadTarotCard(sendData:any) {
-    try {
-    // POST 요청은 body에 실어 보냄
-        const response = await axios.post(`${base_url}tarotcard/gemini/`, {
-                subject: 'Fred',
-                concern: 'Flintstone',
-                selectedCard:""
-            });
-        
-        return response
-        
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+  // POST 요청은 body에 실어 보냄
+    const response = await axios.post(`${base_url}tarotcard/gemini/`, {
+        subject: 'Fred',
+        concern: 'Flintstone',
+        selectedCard:""
+      });
+    
+    return response
+    
+  } catch (e) {
+    console.error(e);
+  }
 }
+
+// 카드목록(일반) 가져오기
+export async function getTarotList() {
+  try {
+    const response = await axios.get(`${base_url}tarotcard/`)
+    
+    return response
+  } catch (e) {
+    console.error(e)
+  }
+}
+// 카드목록(상세) 가져오기
+export async function getTarotDetailList() {
+  try {
+    const response = await axios.get(`${base_url}tarotcard/detail_list/`)
+    
+    return response
+  } catch (e) {
+    console.error(e)
+  }
+}
+// 카드목록(메이저상세) 가져오기
+export async function getTarotMajorList() {
+  try {
+    const response = await axios.get(`${base_url}major_list/`)
+    
+    return response
+  } catch (e) {
+    console.error(e)
+  }
+}
+// 카드목록(마이너상세) 가져오기
+export async function getTarotMinorList() {
+  try {
+    const response = await axios.get(`${base_url}minor_list/`)
+    
+    return response
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+// 카드세부(상세) 가져오기
+export async function getTarotDetail(cardId:number) {
+  try {
+    const response = await axios.get(`${base_url}detail/${cardId}/`);
+    
+    return response;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+
 
 
 // 예시
