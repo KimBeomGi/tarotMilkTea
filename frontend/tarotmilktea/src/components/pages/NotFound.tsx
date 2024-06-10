c3dc76694f48d85e8cf34d91312679b9c129b84c
김범기 <dyd1308@naver.com>
2024-06-10 23:47:38 +0900

    "404 페이지 보완"

--- frontend/tarotmilktea/src/components/pages/NotFound.tsx
+++ frontend/tarotmilktea/src/components/pages/NotFound.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { useNavigate } from 'react-router-dom';
-
+import { useLocation, useNavigate } from 'react-router-dom';
+import "./NotFound.css"
 function NotFound() {
   const navigate = useNavigate()
-
+  const location = useLocation();
+  const [currentUrl, setCurrentUrl] = useState(window.location.href)
+  console.log('Current URL:', window.location.href);
+  const handleRefresh = () => {
+    window.location.reload();
+  }
 
   return (
     <div className="NotFound">
-      <div>
-        <h1>Not Found 페이지</h1>
+      <div className='notFoundContainer'>
+        {/* 구분1 */}
+        <div>
+          <img src={process.env.PUBLIC_URL+"/images/404notfoundImg.png"} alt="" className='notFoundImg'/>
+        </div>
+        {/* 구분2 */}
+        <div>
+          <div>
+            <h1>사이트에 연결할 수 없습니다.</h1>
+            <p>웹페이지 주소가 정확하게 입력되었는지 다시 한 번 확인하시기 바랍니다.<br/>
+            현재 입력한 주소는 {currentUrl}입니다.
+            </p>
+          </div>
+          <button onClick={() => {handleRefresh()}} className='button404 button404maring'>새로고침</button>
+          <button onClick={() => {navigate('/')}} className='button404'>홈으로</button>
+        </div>
       </div>
-      <div>
-        <h2>유효하지 않은 요청입니다.</h2>
-        <p>웹페이지 주소가 정확하게 입력되었는지 다시 한 번 확인하시기 바랍니다.</p>
-      </div>
-      <button onClick={() => {navigate(-1)}}>이전 화면으로</button>
     </div>
   );
 }
