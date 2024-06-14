2c6ef8000482e08c7c309fee13ad2bf8bda06785
김범기 <dyd1308@naver.com>
2024-06-14 19:46:12 +0900

    "로그인 절차 수정, 토큰 여부 확인 후 로그인 진행"

--- frontend/tarotmilktea/src/components/pages/Login.tsx
+++ frontend/tarotmilktea/src/components/pages/Login.tsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 
-import { Outlet, Link, redirect } from "react-router-dom";
+import { Outlet, Link, redirect, useNavigate } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from '../../store/hooks'
 import { decrement, increment, incrementByAmount, decrementByAmount } from '../../store/slices/counter/counterSlice'
 import {KakaoParamsType, GithubParamsType} from '../types/home/homeType'
+import {isTokenVerify} from '../../axios/homeAxios'
 import "./Login.css"
+import Cookies from 'js-cookie';
 
 
 function Login() {
@@ -12,6 +14,38 @@ function Login() {
   const dispatch = useAppDispatch()
   const [incrementN, setIncrementN] = useState<number>(7)
   const [discountN, setDiscountN] = useState<number>(7)
+  const navigate = useNavigate()
+
+  // 토큰 여부를 확인하고 없을 때 진행하기
+  const getIsTokenVerify = async (where:string) => {
+    try {
+      const tmt_token = Cookies.get('tmt_token')
+      let response
+      // 이미 로그인 되어있음.
+      if(tmt_token){
+        response = await isTokenVerify(tmt_token)
+        console.log(response.status)
+        alert('이미 로그인 되어있습니다.')
+        navigate('/')
+      }else{
+        // 로그인 진행
+        if(where === "kakao"){
+          navgiateToKakaoLogin()
+        }else if(where === "github"){
+          navigateGithubLogin()
+        }else if(where === "google"){
+          navigateGoogleLogin()
+        }else{
+          alert('로그인을 진행할 수 없습니다.')
+          navigate('/')
+        }
+      }
+    } catch (error:any) {
+      // console.error('토큰 확인 중 오류 발생:', error);
+      alert('나중에 다시 시도해주세요.');
+      navigate('/')
+    }
+  }
 
   // kakao Login을 위한 코드///////////////////////////////////////////
   const kakaoClientId = process.env.REACT_APP_KAKAO_CLIENT_ID;
@@ -25,7 +59,6 @@ function Login() {
     "response_type" : "code",
   }
   const kParams = new URLSearchParams(kakaoParams).toString()
-
   function navgiateToKakaoLogin(){
     window.location.href = `https://kauth.kakao.com/oauth/authorize?${kParams}`
     // window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
@@ -70,13 +103,19 @@ function Login() {
         <div className='flexCenter'>
           <div
             className='loginButton'
-            onClick={() => {navgiateToKakaoLogin()}}
+            onClick={() => {
+              // navgiateToKakaoLogin()
+              getIsTokenVerify('kakao')
+            }}
             >
             <img src={process.env.PUBLIC_URL+"images/kakaoLoginBtn1.png"} alt="" className='btnImg'/>
           </div>
           <div
             className='loginButton spacebetween loginButtonBorder bgGithub'
-            onClick={() => {navigateGithubLogin()}}
+            onClick={() => {
+              // navigateGithubLogin()
+              getIsTokenVerify('github')
+            }}
             >
               <img src={process.env.PUBLIC_URL+"images/githubLogo1.png"} alt="" className='btnLogoImg'/>
               <span className='loginBtnFont githubTextColor'>Github 로그인</span>
@@ -84,7 +123,10 @@ function Login() {
           </div>
           <div
             className='loginButton spacebetween loginButtonBorder bgGoogle'
-            onClick={() => {navigateGoogleLogin()}}
+            onClick={() => {
+              // navigateGoogleLogin()
+              getIsTokenVerify('google')
+            }}
             >
               <img src={process.env.PUBLIC_URL+"images/googleLogo.png"} alt="" className='btnLogoImg'/>
               <span className='loginBtnFont googleTextColor'>Google 로그인</span>
@@ -92,6 +134,7 @@ function Login() {
           </div>
           <Link to="/" className='nodecoration'>메인 페이지로 이동</Link>
         </div>
+        <div><span onClick={() => {getIsTokenVerify('kakao')}}>눌러보슈</span></div>
       </div>
     </div>
   );
